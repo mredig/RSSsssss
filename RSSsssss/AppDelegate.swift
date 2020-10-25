ea2053e461917b7c7ad0bcd05f266f8da521a981
Michael Redig <mredig@gmail.com>
2020-10-25 18:12:49 -0500

    (feat) added app delegate

---
+++ RSSsssss/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  RSSsssss
+//
+//  Created by Michael Redig on 10/25/20.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+}
