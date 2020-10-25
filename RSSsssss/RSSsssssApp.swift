46d5c195269a5c62bc2209138ec9fa53ba413d4e
Michael Redig <mredig@gmail.com>
2020-10-25 15:16:37 -0500

    initial commit

---
+++ RSSsssss/RSSsssssApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSSsssssApp.swift
+//  RSSsssss
+//
+//  Created by Michael Redig on 10/25/20.
+//
+
+import SwiftUI
+
+@main
+struct RSSsssssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
